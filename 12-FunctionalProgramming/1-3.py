69dfba70e35204d3ad0302729ffe7ec63ef5e611
Bartosz Gondek <236128@student.uek.krakow.pl>
2025-01-10 10:02:15 +0100

    last section

---
+++ 12-FunctionalProgramming/1-3.py
@@ -0,0 +1,6 @@
+def ms_to_kmh(ms):
+  result = ms * 3.6
+  result = int(result)
+  return result 
+print(ms_to_kmh(10))
+print(ms_to_kmh(35))
---
+++ 12-FunctionalProgramming/1-7.py
@@ -0,0 +1,6 @@
+def is_even(number):
+  if number%2 == 0:
+    return "Number is even"
+  else:
+    return "Number is odd"
+print(is_even(5))
